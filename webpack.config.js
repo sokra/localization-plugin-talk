4be21737c7e7727a5d821fd867a52bd28e89a924
Tobias Koppers <tobias.koppers@googlemail.com>
2020-03-04 09:34:41 +0100

    Plugin and module type

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ const HtmlPlugin = require("./lib/HtmlPlugin");
 const LocalizationPlugin = require("./lib/LocalizationPlugin");
 
 module.exports = {
-	plugins: [new HtmlPlugin()],
+	module: {
+		rules: [
+			{
+				test: /loc\.json$/,
+				type: "localization"
+			}
+		]
+	},
+	plugins: [new HtmlPlugin(), new LocalizationPlugin()],
 	experiments: {
 		topLevelAwait: true
 	}
